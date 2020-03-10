bbf0492824f4b3a917023f6005133ece67b661cd
joao front <joao.saousa@tangotech.com.br>
2020-03-10 16:37:24 -0300

    configurando sagas

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
-import {createStore} from  'redux'
+import {createStore, applyMiddleware, compose} from  'redux'
+import createSagaMiddeware from 'redux-saga'
+
 import reducers from './reducers'
+import sagas from './saga'
 import {loadState, savestate}  from './localStorage'
 
-const localStorageState = loadState()
+const localStorageState = loadState();
+const sagaMiddleware = createSagaMiddeware();
+const middewares = [sagaMiddleware];
 
-const store = createStore(
-    reducers,
-    localStorageState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
+const store = createStore(reducers, localStorageState, composeEnhancers(
+    applyMiddleware(...middewares)
+));
+
+sagaMiddleware.run(sagas)
 
 store.subscribe(() =>{
     savestate({list: store.getState().list})
--- src/store/reducers/list.js
+++ src/store/reducers/list.js
@@ -1,6 +1,6 @@
 import { Types } from '../actions/list'
 
-import {createSelector} from 'reselect'
+import { createSelector } from 'reselect'
 import uuidv1 from 'uuid/v1'
 import { ListItemSecondaryAction } from '@material-ui/core';
 import { getAllByPlaceholderText } from '@testing-library/react';
@@ -12,35 +12,46 @@ const initialState = {
 export default function list(state = initialState, action) {
     switch (action.type) {
         case Types.NEW_LIST:
-            return{
+            return {
                 ...initialState, date: getDate()
             }
         case Types.ADD_PRODUCT:
             return {
                 ...state,
                 list: action.list,
+            };
+        case Types.DELETE_PRODUCT:
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== action.productId)
+            }
+        case Types.TOGGLE_PRODUCT:
+            return {
+                ...state,
+                items: toggleItem(state.items, action.productId)
+            }
+        case Types.UPDATE_PRODUCT:
+            return {
+                ...state,
+                list: action.list,
+                items: updateProduct(state.items, action.product)
+            }
+        case Types.GET_IMAGE_SUCCSESS:
+        case Types.GET_IMAGE_FAIL:
+            return {
+                ...state,
                 items: [
                     ...state.items,
-                    { ...action.product, total: getItemTotal(action.product), id: uuidv1(), checked:false}
-                ]
-            };
-            case Types.DELETE_PRODUCT:
-                return{
-                    ...state,
-                    items: state.items.filter(item => item.id !== action.productId)
-                }
-            case Types.TOGGLE_PRODUCT:
-                return{
-                    ...state,
-                    items: toggleItem(state.items, action.productId)
-                }
-                case Types.UPDATE_PRODUCT:
-                    return{
-                        ...state,
-                        list: action.list,
-                        items:updateProduct(state.items, action.product)
+                    {
+                        ...action.product,
+                        total: getItemTotal(action.product),
+                        id: uuidv1(),
+                        checked: false,
+                        img: action.img,
                     }
-                    
+                ]
+            }
+
 
         default:
             return state;
@@ -54,31 +65,31 @@ function getItemTotal(product) {
 function updateProduct(items, product) {
     const index = items.findIndex(item => item.id === product.id);
     return [
-      ...items.slice(0, index),
-      { ...product, total: getItemTotal(product) },
-      ...items.slice(index + 1),
-    ];  
-  }
-function toggleItem(items, productId){
+        ...items.slice(0, index),
+        { ...product, total: getItemTotal(product) },
+        ...items.slice(index + 1),
+    ];
+}
+function toggleItem(items, productId) {
     const index = items.findIndex(item => item.id === productId);
-    return[
+    return [
         ...items.slice(0, index), // tods os items antes do item ser modificado
-        {...items[index], checked: !items[index].checked }, // item atualizado
-       ...items.slice(index + 1) //todos os items do item ser modificado
+        { ...items[index], checked: !items[index].checked }, // item atualizado
+        ...items.slice(index + 1) //todos os items do item ser modificado
     ];
 }
 
-function getDate(){
+function getDate() {
     const date = new Date();
-    const options = {year: 'numeric', month: '2-digit', day: '2-digit'}
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' }
     return date.toLocaleDateString('pt-br', options)
 }
 // Essas funçoes vão fazer os items do hoem funcionar items restantes, items comprados, e total
 // obs todas foram criadas ultilizando o componente creatorSelect do reselect
 
 //essa faz o total funcionar
-export  const getListTotal = createSelector(
-    state => state.list.items, 
+export const getListTotal = createSelector(
+    state => state.list.items,
     items => {
         /* este console serve para saber como o selector do redux funciona */
         console.log('get items total')
